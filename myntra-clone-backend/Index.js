f892274f30834e56fb1bc688f0beb78f69651694
Vishnu ES <55495318+Vishnu-es@users.noreply.github.com>
2022-03-21 22:27:00 +0530

    Setting Up MongoDB

--- myntra-clone-backend/Index.js
+++ myntra-clone-backend/Index.js
@@ -1,11 +1,24 @@
-const express=require("express");
-const port=process.env||5000;
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
 
-app.route("/").get((req,res)=>{
-    res.json("Server is up")
-})
+const port = process.env.port || 5000;
+const app = express();
 
-app.listen(port,"0.0.0.0",()=>{
-    console.log("Server Running",port);
-})
+require("dotenv").config();
+
+mongoose.connect(
+  `mongodb+srv://immortal:${process.env.clusterPassword}@cluster0.n85h6.mongodb.net/MyntraDB?retryWrites=true&w=majority`
+);
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("DB Connected");
+});
+
+app.route("/").get((req, res) => {
+  res.json("Server is up");
+});
+
+app.listen(port, () => {
+  console.log("Server Running", port);
+});
